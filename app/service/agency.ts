32d47e8d0d93bdd055460385643caa0c606a9f29
Yves-Developer <yvesmugisha09@gmail.com>
2025-05-16 23:58:50 +0200

    # [Implemented] Feedback [Plan/Option]

--- app/service/agency.ts
+++ app/service/agency.ts
@@ -2,5 +2,6 @@ import Agency, { agencyPayloadType } from "@/models/Agency";
 export const createAgency = async (payload: agencyPayloadType) => {
   const agency = new Agency(payload);
   await agency.save();
+
   return agency;
 };
