3b09da8900d3543631e6262c4f114b769627347d
Yves-Developer <yvesmugisha09@gmail.com>
2025-05-16 23:05:10 +0200

    [Implement] Google Authetication

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,19 @@
 import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
